57c54d94d881c7c32f69b11d8b347a247df8fc74
Valeriy Oleshivec <v-oleshivec@inbox.ru>
2024-02-24 19:43:44 +0300

    add smooth animation for books

--- src/components/ui/Book/Book.tsx
+++ src/components/ui/Book/Book.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useEffect, useLayoutEffect, useState } from "react";
 import styles from "./Book.module.scss";
 import { Typography } from "components/ui/Typography";
 import { TypographyType } from "components/ui/Typography/Typography";
+import { classNames } from "utils/classNames";
 interface BookProps {
   title: string;
   category: string[];
@@ -10,12 +11,19 @@ interface BookProps {
 }
 
 export const Book = (props: BookProps) => {
+  const [stylesEffect, setStylesEffect] = useState(false);
   const { title, category, authors, image } = props;
   const slicedTitle = title?.length > 45 ? `${title.slice(0, 40)}...` : title;
   const authorsLength = authors?.length - 1;
   const authorText = authors?.join(", ");
+  useLayoutEffect(() => {
+    setTimeout(() => setStylesEffect(true), 100);
+  }, []);
+  const mods = {
+    visible: stylesEffect,
+  };
   return (
-    <div className={styles.book}>
+    <div className={classNames(styles.book, mods, [])}>
       <div className={styles.imageBlock}>
         <img
           src={
